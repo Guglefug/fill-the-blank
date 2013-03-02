3fad705a52ade53764466c711ec8ef70026c1f31
Suzanna Davis <suzanna@guildford.co.nz>
2013-03-02 15:54:28 +1300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FillTheBlank::Application.config.secret_token = '5454ca511deb3b97466d0a7ac0de7d56b6e7df8d18ad887465bd16e2db09c7a2ee57a21c86c0fb45427810939fc52a7a90a70f5564931b6033841962158204c0'
